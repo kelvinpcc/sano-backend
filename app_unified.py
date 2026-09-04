33ee65299518913b86c17cf26d0c3cb17e93e6fe
kelvinpcc <148521423+kelvinpcc@users.noreply.github.com>
2026-09-04 12:02:56 +0800

    Update app_unified.py

--- app_unified.py
+++ app_unified.py
@@ -9,8 +9,8 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 from flask import Flask, render_template_string, request, jsonify, send_file, session, redirect, url_for, flash
-from flask_cors import CORS
 from flaskwebgui import FlaskUI
+from flask_cors import CORS
 
 from reportlab.lib import colors
 from reportlab.lib.pagesizes import letter
@@ -534,8 +534,9 @@ plt.rcParams['font.sans-serif'] = ['DengXian', 'Aptos', 'SimHei', 'Arial Unicode
 plt.rcParams['axes.unicode_minus'] = False
 
 app = Flask(__name__)
-CORS(app)
 app.secret_key = 'bioarchitec_sano_2026'
+CORS(app, resources={r"/api/*": {"origins": "*"}})
+
 
 AUTH_FILE = 'login.csv'
 LOG_FILE = 'logs.csv'
